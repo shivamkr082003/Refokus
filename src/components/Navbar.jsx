20be98c354f1eab4f407a514f0428497985fcf5f
“Shivam <shivam6132@gmail.com>
2025-09-01 02:03:41 -0700

    Adjust margin-top of the indicator in Navbar component for better alignment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
        <div className='links flex gap-14 ml-20'>
             {["Home","Work","Culture","" , "News"].map((elem, index) => (
                elem.length === 0 ? <span className='w-[1px] h-5 bg-zinc-700'></span> : (  <a href="" className="text-white  text-sm flex items-center gap-1 font-regular">
-                {index === 1 && (<span style={{boxShadow: "0 0 0.45em #00FF19"}} className='inline-block w-1 h-1 rounded-full bg-green-500 mt-2'></span>) }
+                {index === 1 && (<span style={{boxShadow: "0 0 0.45em #00FF19"}} className='inline-block w-1 h-1 rounded-full bg-green-500 mt-1'></span>) }
                 {elem}
             </a> )
             ))}
