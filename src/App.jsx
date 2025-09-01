5bec48570ac0d0cb1b9494c2d0f19ec8725c4731
“Shivam <shivam6132@gmail.com>
2025-08-31 22:34:19 -0700

    Add React components and integrate react-icons; remove unused App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
+import React from 'react'
+import Navbar from './components/Navbar'
+import Work from './components/Work'
+import Stripes from './components/Stripes'
 
 function App() {
-  
-
   return (
-    <>
-    
-        
-    </>
+    <div className='w-full h-screen text-white bg-zinc-900'>
+      <Navbar/>
+      <Work/>
+      <Stripes/>
+    </div>
   )
+  
 }
 
 export default App
+
